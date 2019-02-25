b2061c84041677ed6febc28e1fe444a608a791a7
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-25 20:58:36 -0300

    broken action scripts added

--- public/javascript/form.js
+++
@@ -1,9 +0,0 @@
-var Name = document.getElementById('form-name').value 
-var Type = document.getElementById('form-type').value
-
-console.log(Name)
-
-if(typeof(Storage) !== "undefined") {
-    sessionStorage.setItem("name", Name)
-    var name = document.getElementById('name-out').innerHTML =   
-}
--- public/javascript/interaction.js
+++ public/javascript/interaction.js
@@ -1,12 +1,46 @@
-function Increase (id, val, inc) {
-    this.val = this.val + this.inc
-    switch(this.id) {
+function Increase (id, inc) {
+    switch(id) {
         case 1:
-            var affinityw = document.getElementById('')
+            var affinityw = document.getElementById('1')
+            var items = affinityw.getElementsByClassName('fas')
 
-            for (var i; affinityw.lenght; i++) {
-                i
-            }
+            for(var i; i < items.length; i++) {
+                if(items[i].className.includes('affinity-active')) {
+                    items[inc + i].className('affinity-active')
+                }
+            } 
+        break
+ 
+        case 2:
+        break
+
+        case 3:
+        break
+
+        case 4:
+            var affinityw = document.getElementById('4')
+                var items = affinityw.getElementsByTagName('i')
+
+                for(var i; i < items.length; i++) {
+                    if(items[i].className.includes('hunger-active')) {
+                        items[inc - i].classList.remove('hunger-active')
+                    }
+                } 
+        break
+    }
+}
+
+function Decrease (id, inc) {
+    switch(id) {
+        case 1:
+            var affinityw = document.getElementById('1')
+            var items = affinityw.getElementsByTagName('i')
+
+            for(var i; i < items.length; i++) {
+                if(items[i].className.includes('affinity-active')) {
+                    items[inc - i].classList.remove('affinity-active')
+                }
+            } 
         break
  
         case 2:
@@ -20,25 +54,9 @@ function Increase (id, val, inc) {
     }
 }
 
-function Decrease (id, val) {
-    val = val - 1 
 
+function FoodClick() {
+    Increase(1, 1)
+    Increase(4, 0)
+    Decrease(4, 0)
 }
-
-function State (state) {
-    
-
-}
-
-$(document).ready( function() {
-    var 
-        affinity = 1,
-        health = 1,
-        happiness = 1,
-        hunger = 4
-
-   $('#br-1').click(function() {
-        Increase('',affinity,2)
-        Decrease()
-    })
-})
