4ba33a5fd982197b319be667b932148b875f7cff
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-25 09:27:08 -0300

    interaction script added

---
+++ public/javascript/interaction.js
@@ -0,0 +1,12 @@
+$(document).ready( function() {
+    var 
+        affinity = 1,
+        health = 1,
+        happynes = 1,
+        hunger = 4
+
+    
+    $('#br-1').click(function() {
+        
+    })
+})
