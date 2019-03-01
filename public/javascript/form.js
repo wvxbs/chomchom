7d4a46a4b643456046b7a44eff4b6658232427d6
wvxbs <gabriel.ferreira7854@gmail.com>
2019-02-28 22:28:52 -0300

    grr

--- public/javascript/form.js
+++ public/javascript/form.js
@@ -1,9 +1,27 @@
+var name, type, img, desc, div, receive
 function addName() {
     name = document.getElementById('form-name').value
     type = document.getElementById('form-type').value
-    
+    desc = document.getElementById('desc').value
+    img = document.getElementById('form-img').value
+    div = document.getElementById('img')
+    receive = 2gr
+
+    if(receive == 1) {
+        var f1 = new Image(256, 256)
+        f1.src = '../images/edward1.jpg'
+        div.appendChild(f1)
+    } else if(receive == 2) {   
+            var f2 = new Image(256, 256)
+            f2.src = '../images/edward2.jpg'
+            div.appendChild(f2)
+        } else if (receive == 3) {
+                var f3 = new Image(256,256)
+                f3.src = '../images/sheldon1.jpg'
+                div.appendChild(f3)
+            }
 }
 
-document.getElementById('name-out').innerHTML = 'Cuide do ' + name  +  '!'
 document.getElementById('type-out').innerHTML = 'O ' + type + ''
-
+document.getElementById('name-out').innerHTML = 'Cuide do ' + name  +  '!'
+document.getElementById('desc-out').innerHTML = desc
